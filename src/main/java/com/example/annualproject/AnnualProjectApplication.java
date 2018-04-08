e5739ae789edaa389830eea78d6fe13cdbefbf60
ScNicolas <sirac.nicolas@gmail.com>
2018-04-08 15:46:13 +0200

    UserCorrection

---
+++ src/main/java/com/example/annualproject/AnnualProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example.annualproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnualProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnualProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/annualproject/userApi/Role.java
@@ -0,0 +1,4 @@
+package com.example.annualproject.userApi;
+
+public class Role {
+}
