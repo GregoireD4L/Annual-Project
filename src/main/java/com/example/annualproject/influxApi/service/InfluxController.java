d4efe01239454d567124e02e3e3190b126a1b8f9
Nicolas <nicolas.sirac@orange.fr>
2018-05-14 16:14:15 +0200

    Instable Filters

--- src/main/java/com/example/annualproject/influxApi/service/InfluxController.java
+++ src/main/java/com/example/annualproject/influxApi/service/InfluxController.java
@@ -4,16 +4,15 @@ import com.example.annualproject.influxApi.EcgChannelOnePoint;
 import com.example.annualproject.influxApi.InfluxDBReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.Instant;
 import java.util.List;
 
 @CrossOrigin
 @Controller
-@RequestMapping("/ecg1")
-
+@RequestMapping("/data")
+@RestController
 public class InfluxController {
 
         private final InfluxDBReader influxDBReader;
@@ -23,14 +22,20 @@ public class InfluxController {
             this.influxDBReader=influxDBReader;
         }
 
-        @RequestMapping(value = "/getPoints", method = RequestMethod.GET)
-        public List<EcgChannelOnePoint> getPoints() throws Exception {
-            System.out.println("in");
-            return influxDBReader.readECGChannel1();
+        @GetMapping(value = "/getECG1Past")
+        public List<EcgChannelOnePoint> getPointsBetweenTime(@RequestParam String id, @RequestParam Instant beginning, @RequestParam Instant ending ) throws Exception {
+            return influxDBReader.readECGChannel1BeetweenTime(id, beginning,ending);
 
             //  return "redirect:/fichier";
 
         }
+    @GetMapping(value = "/getECG1")
+    public List<EcgChannelOnePoint> getPoints(@RequestParam String id) throws Exception {
+        return influxDBReader.readECGChannel1(id);
+
+        //  return "redirect:/fichier";
+
+    }
 
 
     }
