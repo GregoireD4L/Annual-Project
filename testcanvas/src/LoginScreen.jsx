9e135916d0183fd690adde82bc084bdab3e31e40
Yousria <yousria.benchadi@gmail.com>
2018-06-30 17:06:15 +0200

    added validation functions for inputs

--- testcanvas/src/LoginScreen.jsx
+++ testcanvas/src/LoginScreen.jsx
@@ -36,7 +36,7 @@ const styles = {
     cardRegister: {
         float: "none",
         margin: "0 auto",
-        marginTop: '150px',
+        marginTop: '100px',
         maxWidth: 400,
         maxHeight: 400,
     },
@@ -48,6 +48,7 @@ const styles = {
     textFields: {
         marginTop: 10,
         display: "block",
+        width: 200,
     },
 };
 
@@ -63,6 +64,10 @@ class LoginScreen extends Component {
             welcome: true,
             loginState: false,
             registerState: false,
+            emailValidation: true,
+            passwordValidation: true,
+            firstNameValidation: true,
+            lastNameValidation: true,
         };
 
         this.handleLoginState = this.handleLoginState.bind(this);
@@ -88,24 +93,59 @@ class LoginScreen extends Component {
         });
     }
     handleChangeEmail(event){
-        this.setState({
-            email: event.target.value,
-        });
+        var regExpEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(regExpEmail.test(String(event.target.value).toLowerCase())){
+            this.setState({
+                email: event.target.value,
+                emailValidation: true,
+            });
+        }else{
+            this.setState({
+                email: event.target.value,
+                emailValidation: false,
+            });
+        }
     }
     handleChangePassword(event){
-        this.setState({
-            password: event.target.value,
-        });
+        if(event.target.value.length < 8 || event.target.value.length > 20){
+            this.setState({
+                password: event.target.value,
+                passwordValidation: false,
+            });
+        }else{
+            this.setState({
+                password: event.target.value,
+                passwordValidation: true,
+            });
+        }
+
     }
     handleChangeFirstName(event){
-        this.setState({
-            firstName: event.target.value,
-        });
+        if(event.target.value.length !== 0){
+            this.setState({
+                firstName: event.target.value,
+                firstNameValidation: true,
+            });
+        }else{
+            this.setState({
+                firstName: event.target.value,
+                firstNameValidation: false,
+            });
+        }
+
     }
     handleChangeLastName(event){
-        this.setState({
-            lastName: event.target.value,
-        });
+        if(event.target.value.length !== 0){
+            this.setState({
+                lastName: event.target.value,
+                lastNameValidation: true,
+            });
+        }else{
+            this.setState({
+                lastName: event.target.value,
+                lastNameValidation: false,
+            });
+        }
     }
     handleCancel(event){
         this.setState({
@@ -114,20 +154,23 @@ class LoginScreen extends Component {
             registerState: false,
         })
     }
-    handleRegister(event){
-        firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.error(errorMessage);
-        });
-        //connecté
-        var user = firebase.auth().currentUser;
-        if(user){
-            var userId = user.uid;
-            this.writeUserData(userId, this.state.firstName,this.state.lastName,this.state.email);
-        }
 
+    handleRegister(event){
+        if(this.state.emailValidation === true && this.state.passwordValidation === true
+            && this.state.firstNameValidation === true && this.state.lastNameValidation === true) {
+            firebase.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
+                // Handle Errors here.
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.error(errorMessage);
+            });
+            //connecté
+            var user = firebase.auth().currentUser;
+            if (user) {
+                var userId = user.uid;
+                this.writeUserData(userId, this.state.firstName, this.state.lastName, this.state.email);
+            }
+        }
     }
 
     writeUserData(userId, firstName, lastName ,email) {
@@ -164,6 +207,7 @@ class LoginScreen extends Component {
                 </CardContent>
             </Card>;
         }else if(this.state.loginState){
+            const isEnabled = this.state.email.length > 0 && this.state.password.length > 0;
             cardRender = <Card className={classes.cardLogin}>
                 <CardContent>
                     <Typography variant="title" color="inherit">Sign in</Typography>
@@ -171,43 +215,50 @@ class LoginScreen extends Component {
                         className={classes.textFields}
                         label="Enter your email"
                         id="email"
+                        error={!this.state.emailValidation}
                         onChange={(event, value) => this.handleChangeEmail(event, value)}/>
                     <TextField
                         className={classes.textFields}
                         label="Enter your password"
                         type="password"
                         id="password"
+                        error={!this.state.passwordValidation}
                         onChange={(event, value) => this.handleChangePassword(event, value)}/>
                     <CardActions className={classes.actions}>
-                        <Button className={classes.buttons}>Login</Button>
+                        <Button className={classes.buttons} disabled={!isEnabled} onClick={(event) => this.handleLogin(event)}>Login</Button>
                         <Button className={classes.buttons} onClick={(event) => this.handleCancel(event)}>Cancel</Button>
                     </CardActions>
                 </CardContent>
             </Card>;
         }else if(this.state.registerState){
+            const isEnabled = this.state.email.length > 0 && this.state.password.length > 0;
             cardRender = <Card className={classes.cardRegister}>
                 <CardContent>
                     <Typography variant="title" color="inherit">Sign up</Typography>
                     <TextField
                         className={classes.textFields}
                         label="Enter your First Name"
+                        error={!this.state.firstNameValidation}
                         onChange = {(event) => this.handleChangeFirstName(event)}/>
                     <TextField
                         className={classes.textFields}
                         label="Enter your Last Name"
+                        error={!this.state.lastNameValidation}
                         onChange = {(event) => this.handleChangeLastName(event)}/>
                     <TextField
                         className={classes.textFields}
                         label="Enter your Email"
                         type="email"
+                        error={!this.state.emailValidation}
                         onChange = {(event) => this.handleChangeEmail(event)}/>
                     <TextField
                         className={classes.textFields}
                         type = "password"
                         label="Enter your Password"
+                        error={!this.state.passwordValidation}
                         onChange = {(event) => this.handleChangePassword(event)}/>
                     <CardActions className={classes.actions}>
-                        <Button className={classes.buttons} onClick={(event => this.handleRegister(event))}>Register</Button>
+                        <Button className={classes.buttons} disabled={!isEnabled} onClick={(event => this.handleRegister(event))}>Register</Button>
                         <Button className={classes.buttons} onClick={(event) => this.handleCancel(event)}>Cancel</Button>
                     </CardActions>
                 </CardContent>
